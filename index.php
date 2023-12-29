af1f8c792ef581053441ec9531e9ad19e7b8ba52
Hongbo Liu <hongbo919@gmail.com>
2023-12-29 15:48:56 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<body>
+   <?php include "./header.html" ?>
+
+         <main id="content">
+         <style>.frontmatter{text-align:justify;text-justify:inter-word}img{padding-left:3rem}.section{padding-bottom:0}</style>
+         <section class="section">
+            <div class="container">
+               <div class="content">
+                  <h1 id="welcome-to-the-Liu-lab">Welcome to the Liu Laboratory</h1>
+                  <div class="columns">
+                     <div class="column">
+                        <p class="frontmatter">In the Liu Lab, our goal is to decode the functional genome's role in aging and age-related diseases. Our research is founded on the principle that obtaining a comprehensive understanding of genome regulatory circuits at the system level is crucial for deciphering the underlying causal genetic and molecular factors in human disease.<br><br>To achieve this, we focus on developing statistical and machine learning methods to infer transcriptional regulatory networks and interpret non-coding genetic variation. What makes our research unique is the ability to synergize genome-wide association studies (GWAS) with single-cell multiomics data, enabling us to unveil the mysteries of non-coding DNA, identify causal genes, pinpoint implicated cell types and unearth potential druggable targets associated with aging and various human diseases.<br><br>We collaborate extensively with experimental biologists, locally and globally, validating hypotheses and contributing novel biological insights. Our commitment to bridging computational analyses with experimental validation positions us at the forefront of deciphering the complexities of aging and age-related diseases in biomedical research.</p>
+                     </div>
+                     <div class="column">
+                        <center>
+                           <h1 style="color: #00447E;">Research for Healthy Aging</h1>
+                        </center>
+                        <center>
+                           <h3 style="color: #00447E;">Wellness at every age</h3>
+                        </center>
+                        <center><img src="./image/LiulabResearch3.png" width="600"></center>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+         <style>td.date{font-weight:700}td.news{padding-bottom:.5rem;padding-right:.5rem}td.home{padding-left:20px}</style>
+         <section class="section">
+            <div class="container">
+               <h4 class="subtitle is-4">Lab News</h4>
+               <table style="width:100%">
+                  <tbody>
+                     <tr>
+                        <td class="date">01/01/2024</td>
+                        <td class="home">Open postion: Postdoctoral Associate of Computational Biology. Qualified candidates for the postdoctoral position must hold a Ph.D. degree in Computational Biology and relevant genome sciences. Prior experience in GWAS, Epigenomics, and Single-cell Omics is encouraged.</td>
+                     </tr>
+                     <tr>
+                        <td class="date">01/01/2024</td>
+                        <td class="home">Open postion: Postdoctoral Associate of Molecular Biology. Qualified candidates for the postdoctoral position must hold a Ph.D. degree in Molecular Biology and relevant biological sciences. Prior experience in gene editing and mouse studies is encouraged.</td>
+                     </tr>
+                     <tr>
+                        <td class="date">01/01/2024</td>
+                        <td class="home">Open postion: Graduate students in PhD program: <a href="https://www.urmc.rochester.edu/education/graduate/phd/biomedical-genetics-and-genomics.aspx" target="_blank">Biomedical Genetics and Genomics</a> at University of Rochester.Rotation positions are available.</td>
+                     </tr>
+                     <tr>
+                        <td class="date">01/01/2024</td>
+                        <td class="home">The Liu Lab website (<a href="https://hbliulab.org">https://hbliulab.org</a>) is now live!</td>
+                     </tr>
+                  </tbody>
+               </table>
+            </div>
+         </section>
+      </main>
+
+   <?php include "./footer.html" ?>
+</body>
+
+
